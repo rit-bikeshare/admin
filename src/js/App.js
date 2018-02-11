76a849b3c4bf752c29bf4c801f50698df554199a
Gareth Daunton <gdaunton@hubspot.com>
2018-02-11 17:18:20 -0500

    Initial files.

---
+++ src/js/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { UIBadge } from "@hubspot/canvas";
+
+class App extends Component {
+  render() {
+    return <UIBadge>It's a badge</UIBadge>;
+  }
+}
+
+export default App;
