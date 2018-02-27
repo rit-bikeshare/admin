a3659b2ce09f9829abfe4ea41b749abcd7ed6c6a
Tristan Whitcher <twhitcher@hubspot.com>
2018-02-27 15:19:33 -0500

    Setup eslint config

--- src/js/redux/auth/reducer.js
+++ src/js/redux/auth/reducer.js
@@ -1,12 +1,6 @@
 import { handleActions } from 'redux-actions';
-
+import UserData from './UserData';
 const initialState = new UserData();
+const aa = ';';
 
-export default handleActions(
-  {
-    [ActionTypes.SET_USER_DATA](state, action) {
-      return new UserData(action.payload);
-    }
-  },
-  initialState
-);
+export default handleActions({}, initialState);
