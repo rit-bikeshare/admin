06f548cba897bbf4b8757939a354e6e3e17beecf
Tristan Whitcher <twhitcher@hubspot.com>
2018-04-17 15:46:57 -0400

    export actions explicitly

--- src/js/bikes/actions/bikeActions.js
+++ src/js/bikes/actions/bikeActions.js
@@ -1,1 +1,7 @@
-export { actions as default } from '../';
+import { actions } from '../';
+
+export const create = actions.create;
+export const destroy = actions.destroy;
+export const list = actions.list;
+export const retrieve = actions.retrieve;
+export const update = actions.update;
