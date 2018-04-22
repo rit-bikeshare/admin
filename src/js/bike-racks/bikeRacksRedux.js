897e90fc74662d264ef7dc2208c36551b4f92f94
Tristan Whitcher <tristan.whitcher@gmail.com>
2018-04-21 21:08:54 -0400

    rm casing problem

--- src/js/bike-racks/bikeRacksRedux.js
+++
@@ -1,9 +0,0 @@
-import { all } from '../common-redux/all';
-import BikeRack from './records/BikeRack';
-
-export const name = 'bikeracks';
-export const path = 'admin/bike-racks/';
-export const record = BikeRack;
-export const indexFn = bikerack => bikerack.get('id');
-
-export const { actions, reducer } = all({ name, path, record, indexFn });
