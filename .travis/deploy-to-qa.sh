baad8660258a915c82eb51e11f84e0bd3860f275
Gareth Daunton <gdaunton@gmail.com>
2018-04-26 16:17:16 -0400

    Fix script.

--- .travis/deploy-to-qa.sh
+++ .travis/deploy-to-qa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-ssh $SSH_USERNAME@bikesharedev.rit.edu <<-'ENDSSH'
+ssh -oStrictHostKeyChecking=no $SSH_USERNAME@bikesharedev.rit.edu <<-'ENDSSH'
   curl -s https://api.github.com/repos/rit-bikeshare/admin/releases/latest \
   | grep "browser_download_url.*gz" \
   | cut -d '"' -f 4 \
