529686a22921d9f361cd0ae56742643d66b44bb7
Gareth Daunton <gdaunton@gmail.com>
2018-05-05 00:56:24 -0400

    Fix urls.

--- src/js/api/utils/getBaseRequestUrl.js
+++ src/js/api/utils/getBaseRequestUrl.js
@@ -3,10 +3,10 @@ import getEnv from 'app/utils/getEnv';
 export default function getBaseRequestUrl() {
   switch (getEnv()) {
     case 'dev' || 'qa':
-      return 'https://api.bikesharedev.rit.edu/';
+      return 'https://api.bikesharedev.rit.edu';
     case 'prod':
-      return 'https://api.bikeshare.rit.edu/';
+      return 'https://api.bikeshare.rit.edu';
     default:
-      return 'https://api.bikesharedev.rit.edu/';
+      return 'https://api.bikesharedev.rit.edu';
   }
 }
