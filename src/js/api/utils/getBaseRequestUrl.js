e8e324b6e1e07d09ef1ce9acad004dba60692338
Gareth Daunton <gdaunton@gmail.com>
2018-05-05 00:52:35 -0400

    Update api endpoints.

--- src/js/api/utils/getBaseRequestUrl.js
+++ src/js/api/utils/getBaseRequestUrl.js
@@ -3,10 +3,10 @@ import getEnv from 'app/utils/getEnv';
 export default function getBaseRequestUrl() {
   switch (getEnv()) {
     case 'dev' || 'qa':
-      return 'https://bikesharedev.rit.edu/api';
+      return 'https://api.bikesharedev.rit.edu/';
     case 'prod':
-      return 'https://bikeshare.rit.edu/api';
+      return 'https://api.bikeshare.rit.edu/';
     default:
-      return 'https://bikesharedev.rit.edu/api';
+      return 'https://api.bikesharedev.rit.edu/';
   }
 }
